ecc44e4b5897fc81af05d74d5452dbaf378019de
Ramon Barros <ramonfilipe@gmail.com>
2019-09-04 15:42:47 -0300

    criando a pagina Providers e requisição http

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HelloPage } from '../hello/hello';
 import { AboutPage } from '../about/about';
+
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  products;
+  
+  constructor(
+    public navCtrl: NavController,
+    public httpService: HttpServiceProvider
+  
+  ) {
 
+  }
+
+  ngOnInit() {
+
+    this.httpService.get('products').subscribe(data=> this.products = data);
   }
 
   goToPage() {
